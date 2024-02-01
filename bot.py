d4447ab025edf79c322f3ffff7fd2ebd543a6b88
Ian Maigler <ianmaigler@gmail.com>
2024-01-31 17:11:10 -0800

    initial commit

---
+++ bot.py
@@ -0,0 +1,39 @@
+import discord
+import responses
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+async def send_message(message, user_message, is_private):
+    try:
+        response = responses.handle_response(user_message)
+        await message.author.send(responses) if is_private else await message.channel.send(response)
+    except Exception as e:
+        print(e)
+
+def run_discord_bot():
+    TOKEN = 'MTIwMjM5NzgwNDEyNzk4MTcxOQ.GrWtlp.RWDxs4yDWq8DHiexdhxSI4g8qslDuOAJJKcvGw'
+    client = discord.Client(intents=intents)
+
+    @client.event
+    async def on_ready():
+        print(f'{client.user} is now running!')
+
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
+        
+        username = str(message.author)
+        user_message = str(message.content)
+        channel = str(message.channel)
+
+        print(f"{username} said:'{user_message}' ({channel})")
+
+        if user_message[0] == '?':
+            user_message = user_message[1:]
+            await send_message(message, user_message, is_private=True)
+        else:
+            await send_message(message, user_message, is_private=False)
+
+    client.run(TOKEN)
